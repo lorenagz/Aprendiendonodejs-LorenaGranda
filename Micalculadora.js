9680acb5d443b2ae73505ad1f48f665dfa85d8b6
dylgara <dylgara@gmail.com>
2022-08-12 20:38:39 -0500

    admin

---
+++ Micalculadora.js
@@ -0,0 +1,23 @@
+function Sumar(a,b){
+    return(a+b);
+}
+function Restar(a,b){
+    return(a-b);
+}
+function Multiplicar(a,b){
+    return(a*b);
+}
+function Dividir(a,b){
+    if (b==o){
+        return("no se puede dividir por cero");
+    }
+    else{
+        return
+    }
+}
+
+exports.Sumar = Sumar
+exports.Restar=Restar
+exports.Multiplicar=Multiplicar
+exports.Dividir=Dividir
+//modemon.js index.js
--- index.js
+++ index.js
@@ -4,13 +4,18 @@
 
 const express = require('express');
 const app = express();
+const calc = require('./Micalculadora')
 
 app.get("/", function(req,res){
-    res.send("hola");
+    //res.send("hola");
+    //res.end();
+    console.log(calc.Sumar(4,5));
     res.end();
 })
 
-app.listen(7577);
+app.listen(7577, ()=>{
+    console.log("ejecutando")
+});
 
 
 
