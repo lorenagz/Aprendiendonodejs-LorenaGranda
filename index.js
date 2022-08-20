3068aef5432c28fefc1b319e00eebd52972f3a96
dylgara <dylgara@gmail.com>
2022-08-19 20:29:21 -0500

    19 agosto

--- index.js
+++ index.js
@@ -4,18 +4,32 @@
 
 const express = require('express');
 const app = express();
-const calc = require('./Micalculadora')
+const PORT = process.env.PORT || 9000;
+//const calc = require('./Micalculadora')
+const path =require('path');
 
-app.get("/", function(req,res){
-    //res.send("hola");
-    //res.end();
-    console.log(calc.Sumar(4,5));
-    res.end();
+app.set('views',path.join(__dirname,'/views'));
+app.set('view engine','ejs');
+
+app.use(express.static('./public'));
+
+app.get("/",function(req,res){
+    let usuario = ['Lorena','Granda']
+    var nombre = "usuario LORENA"
+    res.render('pages/index',
+        {name:nombre,
+        user:usuario}
+    );
 })
 
-app.listen(7577, ()=>{
+app.get("/", function(req,res){
+    res.render('pages/index.ejs');
+})
+
+app.listen(PORT, ()=>{
     console.log("ejecutando")
 });
+
 
 
 
